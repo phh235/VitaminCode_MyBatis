d399756e4c315cb772bdb448bde9078419ab5eab
phh235 <hoangaatrox23504@gmail.com>
2024-09-29 11:26:15 +0700

    HuyHoang_Commit01_feat(api)!: Add Exception, ErrorCode and GlobalExceptionHandling

---
+++ src/main/java/com/vitamincode/vitamincode_be/dto/response/ApiResponse.java
@@ -0,0 +1,1 @@
+package com.vitamincode.vitamincode_be.dto.response;import com.fasterxml.jackson.annotation.JsonInclude;import lombok.Builder;import lombok.Getter;import lombok.Setter;@Getter@Setter@Builder@JsonInclude(JsonInclude.Include.NON_NULL)public class ApiResponse<T> {    private Integer status;    private Boolean success;    private T data;    private String message;}
---
+++ src/main/java/com/vitamincode/vitamincode_be/exception/AppException.java
@@ -0,0 +1,1 @@
+package com.vitamincode.vitamincode_be.exception;import lombok.AllArgsConstructor;import lombok.Getter;import lombok.Setter;@Getter@Setter@AllArgsConstructorpublic class AppException extends RuntimeException {    private ErrorCode errorCode;}
